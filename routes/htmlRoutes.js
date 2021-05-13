8465b7d6920d6f95728a831a27e83f808c370d9c
tommylu780 <nhoksun340@gmail.com>
2021-05-13 18:31:43 +1000

    routes, server.js written, add file save.js

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const router = require('express').Router();
+
+// This has to be on the top than the other routers
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../Develop/public/notes.html'));
+});
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../Develop/public/index.html'));
+});
+
+// If no matching route is found default to home page
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../Develop/public/index.html'));
+});
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+// Point to route file
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server is ready on port ${PORT}!`);
+})
